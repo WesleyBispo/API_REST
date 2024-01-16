e2788010874baab9d64f234ee92647e4efe83a5d
WesleyBispo <wesley17bispo@gmail.com>
2024-01-16 03:15:00 -0300

    correção CORS

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 import './database';
 
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
 import helmet from 'helmet';
 
 import homeRoute from './routes/homeRoute';
@@ -10,6 +10,7 @@ import userRoute from './routes/userRoute';
 import tokenRoute from './routes/tokenRoute';
 import studentRoute from './routes/studentRoute';
 import imageRoute from './routes/imageRoute';
+
 class App {
   constructor() {
     this.app = express();
@@ -20,7 +21,7 @@ class App {
   middlewares() {
     this.app.use(cors({ origin: '*' }));
     this.app.use(helmet());
-    this.app.use(express.static(`${__dirname}/uploads/`));
+    this.app.use(express.static(`${__dirname}/uploads/`, { setHeaders: (res) => res.set('Cross-Origin-Resource-Policy', 'cross-origin') }));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
   }
